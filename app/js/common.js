70c9bb5f22f0cebcaf714aa2c5f90a08e5dc355d
master <rudolifrudolif@gmail.com>
2020-03-05 14:30:40 +0200

    page about commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,24 @@
 $(function() {
 
+
+//------------------------------acardeon---------------------------
+  $(".block__content").slideUp("slow");
+  $(".block").first().addClass('active');
+  $(".active .block__content").slideDown("slow");
+
+  $(".block__header").on("click", function(){
+    if ($(this).parent().hasClass('active')) {
+      $(this).parent().removeClass('active');
+      $(".block__content").slideUp("slow");
+    }
+    else {
+      $(".active .block__content").slideUp("slow");
+      $(".active").removeClass('active');
+      $(this).parent().addClass('active');
+      $(".active .block__content").slideDown("slow");
+    }
+  });
+
 //------------------------------гамбургер-----------------------------
   $('.hamburger').click(function() {
     $(this).toggleClass('hamburger--active');
@@ -73,71 +92,6 @@ $(function() {
     scrolllock: true,
     transition: 'all 0.3s'
   });
-
-
-//------------------------------------form-------------------------------------------
-  $('input[type="tel"]').mask('+0 (000) 000-00-00');
-
-  jQuery.validator.addMethod("phoneno", function(phone_number, element) {
-     return this.optional(element) || phone_number.match(/\+[0-9]{1}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}/);
-  }, "Введите Ваш телефон");
-
-  $(".form").each(function(index, el) {
-    $(el).addClass('form-' + index);
-
-    $('.form-' + index).validate({
-      rules: {
-        phone: {
-          required: true,
-          phoneno: true
-        },
-        name: 'required',
-      },
-      messages: {
-        name: "Введите Ваше имя",
-        phone: "Введите Ваш телефон",
-        mail: "Введите Вашу почту",
-        comments: "Введите комментарий",
-        position: "Введите должность",
-        company: "Введите название компании",
-        addres: "Введите адресс",
-      },
-      submitHandler: function(form) {
-        var t = {
-          name: jQuery('.form-' + index).find("input[name=name]").val(),
-          phone: jQuery('.form-' + index).find("input[name=phone]").val(),
-          mail: jQuery('.form-' + index).find("input[name=mail]").val(),
-          comments: jQuery('.form-' + index).find("input[name=comments]").val(),
-          rated: jQuery('.form-' + index).find("input[name=rated]").val(),
-          number: jQuery('.form-' + index).find("input[name=number]").val(),
-          time: jQuery('.form-' + index).find("input[name=time]").val(),
-          reservation: jQuery('.form-' + index).find("input[name=reservation]").val(),
-          textarea: jQuery('.form-' + index).find("textarea[name=textarea]").val(),
-          position: jQuery('.form-' + index).find("input[name=position]").val(),
-          company: jQuery('.form-' + index).find("input[name=company]").val(),
-          addres: jQuery('.form-' + index).find("input[name=addres]").val(),
-          subject: jQuery('.form-' + index).find("input[name=subject]").val(),
-        };
-        ajaxSend('.form-' + index, t);
-      }
-    });
-
-  });
-
-  function ajaxSend(formName, data) {
-    jQuery.ajax({
-      type: "POST",
-      url: "sendmail.php",
-      data: data,
-      success: function() {
-        $(".modal").popup("hide");
-        $("#thanks").popup("show");
-        setTimeout(function() {
-          $(formName).trigger('reset');
-        }, 2000);
-      }
-    });
-  };
 
 });
 
